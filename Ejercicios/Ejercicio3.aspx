68af0bbde682de981c050799ef94eda88c877b6d
DennisMartel <marteldennis05@gmail.com>
2024-03-16 12:20:32 -0600

    Ejercicio numero 3 finalizado

--- Ejercicios/Ejercicio3.aspx
+++ Ejercicios/Ejercicio3.aspx
@@ -4,12 +4,21 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>calculo de descuento basado en cantidad de articulos</title>
+    <link rel="stylesheet" href="Content/Styles/ejercicio3.css" />
 </head>
 <body>
-    <form id="form1" runat="server">
-        <div>
-        </div>
-    </form>
+    <div class="page">
+        <form id="form1" runat="server" class="form">
+            <div>
+                <p>El precio de cada articulo es de $10.00</p>
+                <br />
+                <label class="form-label" id="lbl_cantidad">Ingresa la cantidad de articulos</label>
+                <asp:TextBox ID="txt_cantidad" runat="server" Placeholder="Ingresa la cantidad de articulos" CssClass="form-input"></asp:TextBox>
+                <asp:Button ID="btn_calcular" runat="server" Text="Calcular descuento" CssClass="btn-calcular" OnClick="btn_calcular_Click" />
+                <asp:Label ID="lbl_resultado" runat="server" CssClass="result" Visible="false"></asp:Label>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
--- Ejercicios/Ejercicio3.aspx.cs
+++ Ejercicios/Ejercicio3.aspx.cs
@@ -13,5 +13,54 @@ namespace Ejercicios
         {
 
         }
+
+        protected void btn_calcular_Click(object sender, EventArgs e)
+        {
+            lbl_resultado.Visible = true;
+
+            try 
+            {
+                int cantidad = Convert.ToInt32(txt_cantidad.Text);
+                double descuento = 0;
+                double totalPagar = 0;
+                double precioArticulo = 10.00;
+
+                if (cantidad >= 1 && cantidad <= 5)
+                {
+                    descuento = 0.10;
+                }
+
+                if (cantidad >= 6 && cantidad <=10)
+                {
+                    descuento = 0.20;
+                }
+
+                if (cantidad >= 11 && cantidad <= 15)
+                {
+                    descuento = 0.30;
+                }
+
+                if (cantidad >= 16 && cantidad <= 20)
+                {
+                    descuento = 0.40;
+                }
+
+                if (cantidad > 20)
+                {
+                    descuento = 0.50;
+                }
+
+                totalPagar = precioArticulo * cantidad * (1 - descuento);
+
+                lbl_resultado.ForeColor = System.Drawing.Color.Green;
+                lbl_resultado.Text = $@"Descuento aplicado: {descuento * 100}% <br>
+                                        Total a pagar: ${totalPagar:0.00}";
+            } 
+            catch (FormatException ex) 
+            {
+                lbl_resultado.ForeColor = System.Drawing.Color.Red;
+                lbl_resultado.Text = $@"Error: {ex.Message}";
+            }
+        }
     }
 }
